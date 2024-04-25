ba0f5cfbb8fa40f7ac7ccef8c725b1f5f55508b3
Yuvraj <devyuvraj883@gmail.com>
2024-04-25 06:30:04 +0530

    Added Hero Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import {Routes, Route} from 'react-router-dom';
 
@@ -5,7 +6,7 @@ import {Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <>
-    <Navbar/>
+    <Hero/>
     </>
   );
 }
