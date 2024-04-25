b99d175f3acfd4573f179aaea1e542a17b927773
Yuvraj <devyuvraj883@gmail.com>
2024-04-25 07:55:17 +0530

    Refactored

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import {Routes, Route} from 'react-router-dom';
+import TourCard from "./components/TourCard";
 
 
 function App() {
   return (
     <>
     <Hero/>
+    <div className="flex flex-wrap">
+    <TourCard/>
+    <TourCard/>
+    <TourCard/>
+
+    </div>
     </>
   );
 }
