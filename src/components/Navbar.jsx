a77ed23de6bce54211108e5402dada89d697e84c
Yuvraj <devyuvraj883@gmail.com>
2024-04-25 06:29:15 +0530

    Updated style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { FaSearchLocation } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { IoRocket } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between px-8 items-center h-16">
       <div className="flex items-center">
-        <span className="text-xl font-bold">ExpoCosmo</span>
-        <img src="./images/rocket.png" alt="icon" className="h-8 mt-2" />
+        <span className="text-xl font-bold text-white">ExpoCosmo</span>
+        {/* <img src="./images/rocket.png" alt="icon" className="h-8 mt-2" /> */}
+        <IoRocket className="text-white text-2xl ml-1" />
 
       </div>
       <ul className="flex space-x-4">
@@ -36,7 +38,7 @@ const Navbar = () => {
           </Link>
         </li>
       </ul>
-      <div className="w-60 h-12 bg-gray-100 flex  items-center rounded-l-full rounded-r-full px-4 py-2">
+      <div className="w-60 h-12 mt-2 bg-gray-100 flex  items-center rounded-l-full rounded-r-full px-4 py-2">
         <FaSearchLocation className="ml-auto" />
       </div>
     </div>
