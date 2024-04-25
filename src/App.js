0058b4d9c84af85ffea100fb0d0c9856bea83ecb
Yuvraj <devyuvraj883@gmail.com>
2024-04-25 06:13:32 +0530

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
+import {Routes, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-    
+    <Navbar/>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { FaSearchLocation } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between px-8 items-center h-16">
+      <div className="flex items-center">
+        <img src="./images/rocket.png" alt="icon" className="h-8 mr-2" />
+        <span className="text-xl font-bold">ExpoCosmo</span>
+      </div>
+      <ul className="flex space-x-4">
+        <li>
+          <Link to="/Home" className="text-gray-600 hover:text-black">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/About" className="text-gray-600 hover:text-black">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/Travel" className="text-gray-600 hover:text-black">
+            Travel
+          </Link>
+        </li>
+        <li>
+          <Link to="/Destinations" className="text-gray-600 hover:text-black">
+            Destinations
+          </Link>
+        </li>
+        <li>
+          <Link to="/Space" className="text-gray-600 hover:text-black">
+            Space
+          </Link>
+        </li>
+      </ul>
+      <div className="w-60 h-12 bg-gray-100 flex  items-center rounded-l-full rounded-r-full px-4 py-2">
+        <FaSearchLocation className="ml-auto" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
