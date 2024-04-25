9969692436a39fde3cfeeb654284ce0fceb3e5ca
Yuvraj <devyuvraj883@gmail.com>
2024-04-25 08:51:41 +0530

    Updated Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import TourCard from "../components/TourCard";
+import Tours from "../components/Tours";
+
+const Home = ()=>{
+  return(
+    <>
+     <Hero/>
+    <Tours/>
+
+    </>
+  )
+}
+export default Home;
