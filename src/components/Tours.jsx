8cf3378166c8967331a9314700b2ab7c7b4e913f
Shivam Bansal <91490404+shivamb11@users.noreply.github.com>
2024-04-25 16:44:40 +0530

    Add files via upload

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,31 +1,30 @@
 import { useEffect, useState } from "react";
 import TourCard from "./TourCard";
 
-const Tours = ()=>{
-  const url = 'https://space-toursim-api.onrender.com/tours';
+const Tours = () => {
+  const url = "https://space-toursim-api.onrender.com/tours";
   const [tours, setTours] = useState([]);
-  const getTours = async()=>{
+  const getTours = async () => {
     const resp = await fetch(url);
     const data = await resp.json();
     console.log(data);
     setTours(data);
-  }
-  useEffect(()=>{
+  };
+  useEffect(() => {
     getTours();
-  },[]);
+  }, []);
 
-  return(
+  return (
     <>
-    <div className="flex flex-wrap items-center justify-center w-[90%]">
-      {
-        tours.map((tour)=>(
-          <TourCard tour={tour} key={tour.id}/>
-        ))
-      }
-
-    </div>
-
+      <div
+        className="flex flex-wrap items-center justify-center w-[90%]"
+        id="tours-div"
+      >
+        {tours.map((tour) => (
+          <TourCard tour={tour} key={tour.id} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 export default Tours;
