a507dd4fc7ce45c3e986d67968fa49abcabb6339
Yuvraj <devyuvraj883@gmail.com>
2024-04-25 07:54:51 +0530

    Created TourCard

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,8 +1,23 @@
-const TourCard = ()=>{
-  return(
-    <>
-        <div className=""></div>
-    </>
-  )
-}
+const TourCard = () => {
+  return (
+    <div className="flex flex-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 h-auto px-4 py-6 bg-white shadow-md rounded-md m-2">
+      <img
+        className="h-52 w-full object-cover rounded-md mb-4"
+        src="./images/Hero.jpeg"
+        alt="earth"
+      />
+      <div className="flex items-center justify-between mb-2">
+        <h1 className="text-lg font-bold">Earth</h1>
+        <span className="font-semibold text-sm">3D - 2 N</span>
+      </div>
+      <p className="text-gray-800 text-sm mb-4">
+        Some random description about the tour. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <button className="bg-black text-white font-semibold px-4 py-2 rounded-md w-full hover:bg-white hover:text-black border border-black transition duration-300">
+        Know more
+      </button>
+    </div>
+  );
+};
+
 export default TourCard;
